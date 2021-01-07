6e8a4b2ce1c05d883801dcd7945cefbcbf134cf9
darrell-t <68108814+darrell-t@users.noreply.github.com>
2021-01-07 17:40:00 +0800

    Update getThreshold.php

--- SMAQ/BusinessLayer/getThreshold.php
+++ SMAQ/BusinessLayer/getThreshold.php
@@ -1,6 +1,6 @@
 <?php
 	include "../libs/conn.php";
-	$result = mysqli_query($conn,"SELECT FORMAT(maxRange, 2) AS maxRange, FORMAT(minRange, 2) AS minRange FROM sensor_threshold ORDER BY id");
+	$result = mysqli_query($conn,"SELECT FORMAT(maxRange, 2) AS maxRange, FORMAT(minRange, 2) AS minRange FROM sensor_threshold ORDER BY id ASC");
 	while($row = mysqli_fetch_array($result)) {
 		echo $row['maxRange'];
 		echo "\n";
