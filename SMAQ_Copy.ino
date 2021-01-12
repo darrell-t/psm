db0fbbdd74d411032f7185c43b277caa6947c41a
darrell-t <68108814+darrell-t@users.noreply.github.com>
2021-01-12 12:53:59 +0800

    Update Arduino (include pH)

--- SMAQ_Copy.ino
+++ SMAQ_Copy.ino
@@ -7,6 +7,7 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
+#include <SoftwareSerial.h>
 #include <EMailSender.h>
 #include <DHT.h>
 #include <DallasTemperature.h>
@@ -16,21 +17,27 @@
 #define DHTPIN 5
 #define DHTTYPE DHT22
 
+SoftwareSerial fromUno(D6, D5); // RX, TX
 DHT dht(DHTPIN, DHTTYPE);
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
-const char *ssid = "WIFIID";  //WiFi details
+const char *ssid = "WIFI";  //WiFi details
 const char *password = "PW";  //WiFi details
 const int ldrPin = A0;
-EMailSender emailSend("smaqsystest@gmail.com", "PW"); // Email sender account
+EMailSender emailSend("EXAMPLE@gmail.com", "PW"); // Email sender account
 
 struct sensorCondition {
   float readings;
   int ledAlert;
 };
+
 long startTime, currentTime; // Timer
-int waterTempFlag = 1, airTempFlag = 1, lightIntFlag = 1, airHumFlag = 1; // To trigger the send email function
-float wTempMax, wTempMin, aTempMax, aTempMin, aHumMax, aHumMin, lightMax, lightMin; // Sensor Threshold range
+int waterTempFlag = 1, airTempFlag = 1, lightIntFlag = 1, airHumFlag = 1, phFlag = 1; // To trigger the send email function
+float wTempMax, wTempMin, aTempMax, aTempMin, aHumMax, aHumMin, lightMax, lightMin, phMax, phMin; // Sensor Threshold range
+char myData[10] = "";
+const byte numChars = 32;
+char receivedChars[numChars];
+boolean newData = false;
 
 // Function to connect to WiFi
 void connectWifi(){
@@ -53,6 +60,39 @@ void connectWifi(){
   Serial.println(WiFi.localIP());  //IP address of ESP
 }
 
+// Function to get pH from Uno
+void recvWithStartEndMarkers() {
+  static boolean recvInProgress = false;
+  static byte ndx = 0;
+  char startMarker = '<';
+  char endMarker = '>';
+  char rc;
+  
+  while (fromUno.available() > 0 && newData == false) {
+    rc = fromUno.read();
+    
+    if (recvInProgress == true) {
+      if (rc != endMarker) {
+        receivedChars[ndx] = rc;
+        ndx++;
+        if (ndx >= numChars) {
+          ndx = numChars - 1;
+        }
+      }
+      else {
+        receivedChars[ndx] = '\0'; // terminate the string
+        recvInProgress = false;
+        ndx = 0;
+        newData = true;
+      }
+    }
+    
+    else if (rc == startMarker) {
+    recvInProgress = true;
+    }
+  }
+}
+
 // Function to get Sensor Threshold from database
 String readSensorThreshold(){
   //Serial.println("Whole String:  ");
@@ -73,15 +113,51 @@ String getThresholdValue(String payload, char delimiter, int index){
 
   for(int i=0; i<=maxIndex && found<=index; i++){
     if(payload.charAt(i)==delimiter || i==maxIndex){
-        found++;
-        strIndex[0] = strIndex[1]+1;
-        strIndex[1] = (i == maxIndex) ? i+1 : i;
+      found++;
+      strIndex[0] = strIndex[1]+1;
+      strIndex[1] = (i == maxIndex) ? i+1 : i;
     }
   }
-
   return found>index ? payload.substring(strIndex[0], strIndex[1]) : "";
 }
 
+// Function to read pH Level
+struct sensorCondition readphLevel() {
+  String temp;
+  struct sensorCondition phValue;
+    if (newData == true) {
+        temp = receivedChars;
+        phValue.readings = temp.toFloat();
+        if(phValue.readings  < phMin || phValue.readings > phMax){
+          startTime = millis(); // Get number of ms passed since running the program
+          phValue.ledAlert = 1;
+          // Send alert email
+          if(phFlag == 1){
+            EMailSender::EMailMessage message;
+            message.subject = "SMAQ System Alert (pH Level)";
+            message.message = "pH Level is at " + String(phValue.readings);
+            EMailSender::Response resp = emailSend.send("EXAMPLE@gmail.com", message);
+            Serial.println("Sending status: ");
+            //Serial.println(resp.code);
+            //Serial.println(resp.desc);
+            Serial.println(resp.status);
+            phFlag = 0;
+            currentTime = startTime;
+          }
+          else{
+            // Check if interval period has passed before sending another email
+            if(startTime - currentTime >= 300000){
+              phFlag = 1;
+            }
+          }
+      }
+      else{
+        phValue.ledAlert = 0;
+      }
+      newData = false;
+      return phValue;
+    }
+}
 
 // Function to read water temperature
 struct sensorCondition readWaterTemp(float wTempMax, float wTempMin){
@@ -96,7 +172,7 @@ struct sensorCondition readWaterTemp(float wTempMax, float wTempMin){
         EMailSender::EMailMessage message;
         message.subject = "SMAQ System Alert (Water Temperature)";
         message.message = "Water temperature is at " + String(waterTemp.readings) + "°C";
-        EMailSender::Response resp = emailSend.send("smaqsystest@gmail.com", message);
+        EMailSender::Response resp = emailSend.send("EXAMPLE@gmail.com", message);
         Serial.println("Sending status: ");
         //Serial.println(resp.code);
         //Serial.println(resp.desc);
@@ -106,7 +182,7 @@ struct sensorCondition readWaterTemp(float wTempMax, float wTempMin){
       }
       else{
         // Check if interval period has passed before sending another email
-        if(startTime - currentTime >= 60000){
+        if(startTime - currentTime >= 300000){
           waterTempFlag = 1;
         }
       }
@@ -129,7 +205,7 @@ struct sensorCondition readAirTemp(float aTempMax, float aTempMin){
         EMailSender::EMailMessage message;
         message.subject = "SMAQ System Alert (Air Temperature)";
         message.message = "Air Temperature is at " + String(airTemp.readings) + "°C";
-        EMailSender::Response resp = emailSend.send("smaqsystest@gmail.com", message);
+        EMailSender::Response resp = emailSend.send("EXAMPLE@gmail.com", message);
         Serial.println("Sending status: ");
         //Serial.println(resp.code);
         //Serial.println(resp.desc);
@@ -139,7 +215,7 @@ struct sensorCondition readAirTemp(float aTempMax, float aTempMin){
       }
       else{
         // Check if interval period has passed before sending another email
-        if(startTime - currentTime >= 60000){
+        if(startTime - currentTime >= 300000){
           airTempFlag = 1;
         }
       }
@@ -162,7 +238,7 @@ struct sensorCondition readAirHum(float aHumMax, float aHumMin){
         EMailSender::EMailMessage message;
         message.subject = "SMAQ System Alert (Relative Humidity)";
         message.message = "Relative Humidity is at " + String(airHum.readings) + "%";
-        EMailSender::Response resp = emailSend.send("smaqsystest@gmail.com", message);
+        EMailSender::Response resp = emailSend.send("EXAMPLE@gmail.com", message);
         Serial.println("Sending status: ");
         //Serial.println(resp.code);
         //Serial.println(resp.desc);
@@ -172,7 +248,7 @@ struct sensorCondition readAirHum(float aHumMax, float aHumMin){
       }
       else{
         // Check if interval period has passed before sending another email
-        if(startTime - currentTime >= 60000){
+        if(startTime - currentTime >= 300000){
           airHumFlag = 1;
         }
       }
@@ -195,7 +271,7 @@ struct sensorCondition readLightInt(float lightMax, float lightMin){
         EMailSender::EMailMessage message;
         message.subject = "SMAQ System Alert (Light Intensity)";
         message.message = "Light Intensity is at " + String(lightInt.readings) + "lx";
-        EMailSender::Response resp = emailSend.send("smaqsystest@gmail.com", message);
+        EMailSender::Response resp = emailSend.send("EXAMPLE@gmail.com", message);
         Serial.println("Sending status: ");
         //Serial.println(resp.code);
         //Serial.println(resp.desc);
@@ -205,7 +281,7 @@ struct sensorCondition readLightInt(float lightMax, float lightMin){
       }
       else{
         // Check if interval period has passed before sending another email
-        if(startTime - currentTime >= 60000){
+        if(startTime - currentTime >= 300000){
           lightIntFlag = 1;
         }
       }
@@ -217,10 +293,10 @@ struct sensorCondition readLightInt(float lightMax, float lightMin){
 }
 
 // Function to send readings
-void sendReadings(struct sensorCondition waterTemp, struct sensorCondition airTemp, struct sensorCondition airHum, struct sensorCondition lightInt){
+void sendReadings(struct sensorCondition waterTemp, struct sensorCondition airTemp, struct sensorCondition airHum, struct sensorCondition lightInt, struct sensorCondition phValue){
   HTTPClient http;
   String postData;
-  postData = "waterTemp=" + String(waterTemp.readings) + "&airTemp=" + String(airTemp.readings) + "&airHum=" + String(airHum.readings) + "&lightInt=" + String(lightInt.readings);
+  postData = "waterTemp=" + String(waterTemp.readings) + "&airTemp=" + String(airTemp.readings) + "&airHum=" + String(airHum.readings) + "&lightInt=" + String(lightInt.readings) + "&phValue=" + String(phValue.readings);
   http.begin("URL/postReadings.php");
   http.addHeader("Content-Type", "application/x-www-form-urlencoded");
   int httpCode = http.POST(postData);
@@ -232,6 +308,7 @@ void sendReadings(struct sensorCondition waterTemp, struct sensorCondition airTe
 
 void setup() {
   Serial.begin(9600);
+  fromUno.begin(9600);
   connectWifi();
   sensors.begin();
   dht.begin();
@@ -241,8 +318,9 @@ void setup() {
 }
 
 void loop() {
-  struct sensorCondition waterTemp, airTemp, lightInt, airHum;
+  struct sensorCondition waterTemp, airTemp, lightInt, airHum, phValue;
   String payload;
+  recvWithStartEndMarkers();
   payload = readSensorThreshold(); // Assign all sensor threshold values to variable
   wTempMax = getThresholdValue(payload, '\n', 0).toFloat();
   wTempMin = getThresholdValue(payload, '\n', 1).toFloat();
@@ -252,16 +330,19 @@ void loop() {
   aHumMin = getThresholdValue(payload, '\n', 5).toFloat();
   lightMax = getThresholdValue(payload, '\n', 6).toFloat();
   lightMin = getThresholdValue(payload, '\n', 7).toFloat();
+  phMax = getThresholdValue(payload, '\n', 8).toFloat();
+  phMin = getThresholdValue(payload, '\n', 9).toFloat();
   waterTemp = readWaterTemp(wTempMax, wTempMin);
   airTemp = readAirTemp(aTempMax, aTempMin);
   airHum = readAirHum(aHumMax, aHumMin);
   lightInt = readLightInt(lightMax, lightMin);
-  if(waterTemp.ledAlert == 1 || airTemp.ledAlert == 1 || lightInt.ledAlert == 1 || airHum.ledAlert == 1){
+  phValue = readphLevel();
+  if(waterTemp.ledAlert == 1 || airTemp.ledAlert == 1 || lightInt.ledAlert == 1 || airHum.ledAlert == 1 || phValue.ledAlert == 1){
     digitalWrite(2, HIGH);
   }
   else{
     digitalWrite(2, LOW);
   }
-  sendReadings(waterTemp, airTemp, airHum, lightInt);
+  sendReadings(waterTemp, airTemp, airHum, lightInt, phValue);
   delay(5000);
 }
